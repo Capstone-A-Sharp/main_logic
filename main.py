0a5bc3d37c8acf99d456db2fe7508a88184e2d16
Rimi <lora3226@inu.ac.kr>
2025-04-27 19:13:49 +0900

    ✨Feat: calibration 용 스위치 값 받아오기

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from control_logic import SpeedController
 from visualizer import start_visualization, update_image
 from serial_dispatcher import parse_serial_line
 from calibration import Calibration
-import time
 
 # 시리얼 포트 초기화
 ser = connect_arduino()
@@ -13,27 +12,30 @@ calibration = Calibration()
 
 # 센서 상태 정보 저장용 context
 context = {
+    # 기울기
     "pitch": 0.0,
-    "fsr_matrix_left": [[0] * 16 for _ in range(16)],  
-    "fsr_matrix_right": [[0] * 16 for _ in range(16)],  
+    
+    # 전체 16x32 fsr_matrix
+    "fsr_matrix": [[0] * 32 for _ in range(16)],
+    
+    # 좌우 합산 압력
     "left_sum": 0,
     "right_sum": 0,
-    "calib_switch": 0, 
-    "left_min" : 0,
-    "right_min" : 0,
-    "left_max" : 999,
-    "right_max" : 999
+    "left_min": 0,
+    "right_min": 0,
+    "left_max": 999,
+    "right_max": 999,
+    
+    # 캘리브레이션 스위치
+    "calib_switch": 0
 }
 
 def update_wrapper(*args):
-
     fig, ax, cax = start_visualization(context)
 
     buffer = ""
 
     while True:
-        # now = time.time()
-
         # 시리얼 데이터 읽기
         if ser.in_waiting:
             line = ser.readline().decode(errors='ignore').strip()
@@ -43,27 +45,29 @@ def update_wrapper(*args):
             if line == "END":
                 if buffer:
                     try:
-                        result = parse_serial_line(buffer, context)
+                        # 시리얼 통신으로 값 받아오기
+                        parse_serial_line(buffer, context)
+                        
+                        # 실시간 압력센서값 그래프로 나타내기
                         update_image(fig, ax, cax, context)
 
-                        if result:
-                            # 디버깅
-                            print("[Sensor 1 결과] ----------------")
-                            print(f"Pitch 값: {context.get('pitch')}")
+                        # 디버깅
+                        print(f"Pitch 값: {context.get('pitch')}")
+                        print("FSR 매트릭스 값:")
+                        for idx, row in enumerate(context.get('fsr_matrix', [])):
+                            print(f"Row {idx}: {row}")
 
-                            print("FSR 매트릭스 값:")
-                            for idx, row in enumerate(context.get('fsr_matrix', [])):
-                                print(f"Row {idx}: {row}")
+                        # 압력센서 left, right 합 계산
+                        controller.calculate_sum(context)
+                        print(f"[PWM 계산] Left 합: {context.get('left_sum')}, Right 합: {context.get('right_sum')}")
 
-                            controller.calculate_sum(context)
-                            print(f"[PWM 계산] Left 합: {context.get('left_sum')}, Right 합: {context.get('right_sum')}")
-
-                            # calib_switch ON일 경우 : 캘리브레이션 진행
-                            if context.get('calib_switch')==1 :
-                                calibration.calculate_minmax(context)
-                            # 메인 로직 진행
-                            else :
-                                controller.calculate_pwm(context)
+                        # calib_switch ON일 경우 : 캘리브레이션 진행
+                        if context.get('calib_switch')==1 :
+                            calibration.calculate_minmax(context)
+                            
+                        # 메인 로직 진행
+                        else :
+                            controller.calculate_pwm(context)
 
                     except Exception as e:
                         print("[Error] JSON 파싱 중 문제 발생:", e)
@@ -72,7 +76,5 @@ def update_wrapper(*args):
             else:
                 buffer += line
 
-
-# 진입점
 if __name__ == "__main__":
     update_wrapper()
--- serial_dispatcher.py
+++ serial_dispatcher.py
@@ -1,7 +1,5 @@
 import json
 
-import json
-
 def parse_serial_line(line, context):
     try:
         data = json.loads(line)
@@ -9,9 +7,8 @@ def parse_serial_line(line, context):
         print("[Error] JSON 디코딩 실패:", line)
         return None
 
-    # MPU9250 데이터 파싱
+    # 자이로 센서 기울기값
     mpu_data = data.get("MPU9250", {})
-
     context["pitch"] = mpu_data.get("pitch", 0.0)
 
     # FSR 데이터 파싱
@@ -20,15 +17,9 @@ def parse_serial_line(line, context):
     for row_idx in range(16):
         row_key = f"row{row_idx}"
         row_data = fsr_data.get(row_key, [0]*32)
-
-        # rowN 전체를 context에 저장
-        context[f"fsr_row{row_idx}"] = row_data
-
         matrix.append(row_data)
 
-    context["fsr_matrix"] = matrix  # 16x32 매트릭스도 같이 저장
-
-    return {
-        "mpu": mpu_data,
-        "fsr": fsr_data
-    }
+    context["fsr_matrix"] = matrix 
+    
+    # calib_switch 데이터 파싱
+    context["calib_switch"] = data.get("calib_switch", 0)
