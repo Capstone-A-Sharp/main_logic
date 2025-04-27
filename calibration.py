86155b41e59c91878f5ef5a8a8f32ce04610de50
Rimi <lora3226@inu.ac.kr>
2025-04-27 18:58:36 +0900

    ✨Feat: left, right 최솟값, 최댓값 갱신

--- calibration.py
+++ calibration.py
@@ -3,5 +3,16 @@ class Calibration:
         pass
     
     def caculate_minmax(self,context):
-        # context의 min, max 갱신]
-        pass
+        # 최솟값 갱신
+        if context.get('left_min')>context.get('left_sum'):
+            context['left_min']=context.get('left_sum')
+        if context.get('right_min')>context.get('right_sum'):
+            context['right_min']=context.get('right_sum')
+            
+        # 최댓값 갱신
+        if context.get('left_max')<context.get('left_sum'):
+            context['left_max']=context.get('left_sum')
+        if context.get('right_max')<context.get('right_sum'):
+            context['right_max']=context.get('right_sum')
+           
+            
